8bc981bc043b5304a4fb248f0e1084ee077e8c5f
jahid-opu <ziopu05@gmail.com>
2022-02-01 20:58:24 +0600

    AuthContext

--- src/components/App.js
+++ src/components/App.js
@@ -12,22 +12,25 @@ import {
   Routes,
   Link,
 } from "react-router-dom";
+import { AuthProvider } from "../contexts/AuthContext";
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
+      <AuthProvider>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
 
-          <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<Login />} />
 
-          <Route path="/quiz" element={<Quiz />} />
+            <Route path="/quiz" element={<Quiz />} />
 
-          <Route path="/result" element={<Result />} />
-        </Routes>
-      </Layout>
+            <Route path="/result" element={<Result />} />
+          </Routes>
+        </Layout>
+      </AuthProvider>
     </Router>
   );
 }
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,69 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import "../firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+
+      return unsubscribe;
+    });
+  }, []);
+
+  //   Sign up function
+  const signup = async (email, password, userName) => {
+    const auth = getAuth();
+    await createUserWithEmailAndPassword(auth, email, password);
+
+    // update profile
+    await updateProfile(auth.currentUser, { displayName: userName });
+
+    const user = auth.currentUser;
+    setCurrentUser({
+      ...user,
+    });
+  };
+
+  //   Login function
+  const login = (email, password) => {
+    const auth = getAuth();
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  //   Logout function
+  const logout = () => {
+    const auth = getAuth();
+    return signOut(auth);
+  };
+
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+  };
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
