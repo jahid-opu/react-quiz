237ba69373a5525564a40af019f62bc3bac42050
jahid-opu <ziopu05@gmail.com>
2022-02-01 18:18:38 +0600

    routing setup

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,30 @@ import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import Signup from "./pages/Signup";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
-    <Layout>
-      {/* <Home /> */}
-      {/* <Signup /> */}
-      {/* <Login /> */}
-      {/* <Quiz /> */}
-      <Result />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/quiz" element={<Quiz />} />
+
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
