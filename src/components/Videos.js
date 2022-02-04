fb3809427cea0c86fe6d53cd22515853ca69d430
jahid-opu <ziopu05@gmail.com>
2022-02-04 16:28:35 +0600

    data fetching from database

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import classes from "../styles/Videos.module.css";
 import Video from "./Video";
+import useVideoList from "./../hooks/useVideoList";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 const Videos = () => {
+  const [page, setPage] = useState(1);
+  const { loading, error, videos, hasMore } = useVideoList(page);
+
   return (
-    <div className={classes.videos}>
-      <Link to="/quiz">
-        <Video />{" "}
-      </Link>
-      <Link to="/quiz">
-        <Video />{" "}
-      </Link>
-      <Link to="/quiz">
-        <Video />{" "}
-      </Link>
-      <Link to="/quiz">
-        <Video />{" "}
-      </Link>
-      <Link to="/quiz">
-        <Video />{" "}
-      </Link>
-      <Link to="/quiz">
-        <Video />{" "}
-      </Link>
+    <div>
+      {videos.length > 0 && (
+        <InfiniteScroll
+          dataLength={videos.length}
+          hasMore={hasMore}
+          next={() => setPage(page + 8)}
+        >
+          {videos.map((video) =>
+            video.noq > 0 ? (
+              <Link to="/quiz" key={video.youtubeID}>
+                <Video
+                  title={video.title}
+                  id={video.youtubeID}
+                  noq={video.noq}
+                />
+              </Link>
+            ) : (
+              <Video title={video.title} id={video.youtubeID} noq={video.noq} />
+            )
+          )}
+        </InfiniteScroll>
+      )}
+      {!loading && videos.length === 0 && <div className="">No data found</div>}
+      {error && <div className="">Error</div>}
+      {loading && <div className="">Loading...</div>}
     </div>
   );
 };
